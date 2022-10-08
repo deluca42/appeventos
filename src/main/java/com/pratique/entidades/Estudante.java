dd911fa1218c9bc5b64943a29573842591e6d4fa
deluca42@gmail.com <*97178@git>
2022-10-08 10:03:42 -0300

    first commit

---
+++ src/main/java/com/pratique/entidades/Estudante.java
@@ -0,0 +1,33 @@
+package com.pratique.entidades;
+
+
+
+public class Estudante {
+	
+	String nome; 
+	int    idade;
+	String endereco; 
+	
+	public Estudante () {
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+}
---
+++ src/main/java/com/pratique/entidades/Evento.java
@@ -0,0 +1,69 @@
+package com.pratique.entidades;
+
+import java.util.Date;
+
+public class Evento {
+	
+	String nome; 
+	String categoria; 
+	String Descricao;
+	String endereco;
+	int    quantidadePessoas;
+	Date   dataHora;
+	
+	public Evento() {
+		
+	}
+		
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	public String getDescricao() {
+		return Descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		Descricao = descricao;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public int getQuantidadePessoas() {
+		return quantidadePessoas;
+	}
+
+	public void setQuantidadePessoas(int quantidadePessoas) {
+		this.quantidadePessoas = quantidadePessoas;
+	}
+
+	public Date getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(Date dataHora) {
+		this.dataHora = dataHora;
+	}
+	
+
+	
+
+}
